875e1b7b4d39e5a72237b73a44cce87ad573648b
Sam Funk <borley1@gmail.com>
2025-01-16 10:05:30 -0500

    changed original demo to have login view and invoice view for content.

--- WpfDemo/InvoiceView.xaml.cs
+++ WpfDemo/InvoiceView.xaml.cs
@@ -1,4 +1,8 @@
-﻿using System.Text;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -12,18 +16,13 @@ using System.Windows.Shapes;
 namespace WpfDemo
 {
     /// <summary>
-    /// Interaction logic for MainWindow.xaml
+    /// Interaction logic for InvoiceView.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class InvoiceView : UserControl
     {
-        public MainWindow()
+        public InvoiceView()
         {
             InitializeComponent();
-            
-            MainContent.Content = new LoginView();
-
         }
-
-      
     }
 }
